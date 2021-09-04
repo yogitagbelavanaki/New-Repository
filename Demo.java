12ac9e36363ae2cb8449a03875e8d869c5c67c2e
yogitagbelavanaki <90086466+yogitagbelavanaki@users.noreply.github.com>
2021-09-04 15:35:56 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,16 @@
+class Demo
+{
+  static void trapezoid()
+  {
+    int a = 10;
+    int b= 19;
+    int h = 30;
+    double result = (1/(2.0))*(a+b)*h;
+    System.out.println(result);
+   }
+   public static void main(String[] args)
+   {
+     trapezoid();
+
+    }
+ }
